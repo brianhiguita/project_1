767052d89e91a98202e110ed17959e58fbfc4984
Brian Higuita <brian-higuita@hotmail.co.uk>
2018-08-07 07:28:01 +0100

    login & register includes added

--- index.php
+++ index.php
@@ -16,19 +16,7 @@
   <div class="row">
     <div class="col-md-6 register">
 
-      <form class="" action="index.php" method="post">
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="username" placeholder="Enter email">
-        </div>
-
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-        </div>
-
-        <button type="submit" name="submit" class="btn btn-primary">Sign In</button>
-      </form>
+      <?php include "includes/login_form.php"; ?>
 
       <?php
         if ($error_message !== "") {
--- register.php
+++ register.php
@@ -2,38 +2,17 @@
 
 <?php include "includes/nav.php"; ?>
 
-
-
 <?php
 
   register();
 
 ?>
 
-
-
 <div class="container">
   <div class="row">
     <div class="col-md-6 register">
 
-      <form class="" action="" method="post">
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="username" placeholder="Enter email">
-        </div>
-
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-        </div>
-
-        <div class="form-group">
-          <label for="confirmPassworld">Confirm Password</label>
-          <input type="password" class="form-control" id="confirmPassworld" name="confirmPassword" placeholder="Confirm Password">
-        </div>
-
-        <button type="submit" name="submit" class="btn btn-primary">Register</button>
-      </form>
+      <?php include "includes/register_form.php"; ?>
 
       <?php
         if ($error_message !== "") {
